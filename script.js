61bc0479531779bd300afff69b32bd08860dce6d
Sumedha Singh Rathor <sumedharathor@gmail.com>
2024-08-27 22:47:26 +0530

    Loader Animation and Hero Section

---
+++ script.js
@@ -0,0 +1,80 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const counter3 = document.querySelector(".counter-3");
+
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 10; j++) {
+      const div = document.createElement("div");
+      div.className = "num";
+      div.textContent = j;
+      counter3.appendChild(div);
+    }
+  }
+
+  const finalDiv = document.createElement("div");
+  finalDiv.className = "num";
+  finalDiv.textContent = "0";
+  counter3.appendChild(finalDiv);
+
+  function animate(counter, duration, delay = 0) {
+    const numHeight = counter.querySelector(".num").clientHeight;
+    const totalDistance =
+      (counter.querySelectorAll(".num").length - 1) * numHeight;
+
+    gsap.to(counter, {
+      y: -totalDistance,
+      duration: duration,
+      delay: delay,
+      ease: "power3.inOut",
+    });
+  }
+
+  animate(counter3, 5);
+  animate(document.querySelector(".counter-2"), 6);
+  animate(document.querySelector(".counter-1"), 2, 4);
+});
+
+var load_animation = gsap.timeline();
+
+load_animation.to(".digit", {
+  top: "-150px",
+  stagger: {
+    amount: -0.25,
+  },
+  delay: 6,
+  duration: 1,
+  ease: "power4.inOut",
+});
+
+load_animation.to(".loading-screen", {
+  //   transform: "translateY(-100vh)",
+  height: "0px",
+  duration: 0.3,
+  delay: 0.1,
+  stagger: -0.3,
+});
+
+load_animation.from("#web_logo", {
+  y: -60,
+  opacity: 0,
+  delay: 0.5,
+});
+
+load_animation.from(".link", {
+  y: -60,
+  opacity: 0,
+  delay: 0.3,
+  stagger: -0.1,
+});
+
+load_animation.from(".marquee", {
+  y: 100,
+  opacity: 0,
+  delay: 0.1,
+  stagger: -0.05,
+});
+
+load_animation.from(".job", {
+  y: 50,
+  opacity: 0,
+  stagger: 0.2,
+});
