e6109f9953c54c542974f2fa67e8b476a12afd35
Sumedha Singh Rathor <sumedharathor@gmail.com>
2024-10-20 00:02:45 +0530

    Adding certificate folder

--- script.js
+++ script.js
@@ -93,27 +93,27 @@ website_content.addEventListener("click", function (dets) {
 });
 
 // Active Section
-// let sections = document.querySelectorAll("section");
-// let navLinks = document.querySelectorAll(
-//   ".website-content .nav .navbar .link a"
-// );
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll(
+  ".website-content .nav .navbar .link a"
+);
 
-// window.onscroll = () => {
-//   sections.forEach((sec) => {
-//     let top = window.scrollY;
-//     let offset = sec.offsetTop;
-//     let height = sec.offsetHeight;
-//     let id = sec.getAttribute("id");
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-//     if (top >= offset && top < offset + height) {
-//       navLinks.forEach((links) => {
-//         links.classList.remove("active");
-//         document
-//           .querySelector(
-//             ".website-content .nav .navbar .link a[href*=" + id + "]"
-//           )
-//           .classList.add("active");
-//       });
-//     }
-//   });
-// };
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector(
+            ".website-content .nav .navbar .link a[href*=" + id + "]"
+          )
+          .classList.add("active");
+      });
+    }
+  });
+};
