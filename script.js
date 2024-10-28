cd2d38296019eea1f384f7128e0e5e42bed6c840
Sumedha Singh Rathor <sumedharathor@gmail.com>
2024-10-28 12:04:03 +0530

    Counter Animation

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+// Counter Animation
+document.addEventListener("DOMContentLoaded", function () {
+  const counter3 = document.querySelector(".counter-3");
 
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 10; j++) {
+      const div = document.createElement("div");
+      div.className = "num";
+      div.textContent = j;
+      counter3.appendChild(div);
+    }
+  }
+
+  const finalDiv = document.createElement("div");
+  finalDiv.className = "num";
+  finalDiv.textContent = "0";
+  counter3.appendChild(finalDiv);
+
+  function animate(counter, duration, delay = 0) {
+    const numHeight = counter.querySelector(".num").clientHeight;
+    const totalDistance =
+      (counter.querySelectorAll(".num").length - 1) * numHeight;
+
+    gsap.to(counter, {
+      y: -totalDistance,
+      duration: duration,
+      delay: delay,
+      ease: "power3.inOut",
+    });
+  }
+
+  animate(counter3, 5);
+  animate(document.querySelector(".counter-2"), 6);
+  animate(document.querySelector(".counter-1"), 2, 4);
+});
